b85a78aafcd20e48b4b090159bf977a50923cbe8
mirekphd <mirekphd@gmail.com>
2023-02-08 08:33:13 +0000

    +start-tritonserver.sh

---
+++ start-tritonserver.sh
@@ -0,0 +1,1 @@
+docker rm -f triton && docker run -d --rm --name triton -p 8000-8002 -v $(pwd)/model_repository:/model_repository nvcr.io/nvidia/tritonserver:23.01-py3 tritonserver --model-repository=/model_repository && docker logs -f triton
