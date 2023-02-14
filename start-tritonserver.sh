5d85c733682eccd8ab1de55cbeb1c75d809d247c
mirekphd <mirekphd@gmail.com>
2023-02-14 19:54:14 +0000

    start srv sh: -> multi line cmds

--- start-tritonserver.sh
+++ start-tritonserver.sh
@@ -1,1 +1,3 @@
-docker rm -f triton && docker run -d --rm --name triton -p 8000-8002 -v $(pwd)/model_repository:/model_repository nvcr.io/nvidia/tritonserver:23.01-py3 tritonserver --model-repository=/model_repository && docker logs -f triton
+docker rm -f triton && \
+docker run -d --rm --name triton -p 8000-8002 -v $(pwd)/model_repository:/model_repository nvcr.io/nvidia/tritonserver:23.01-py3 tritonserver --model-repository=/model_repository && \
+docker logs -f triton
